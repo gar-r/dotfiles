f551525d4dc11672069d1c9e27924774865b4a20
Richard Garai <garric@okki.hu>
2025-10-01 18:41:05 +0200

    clear config

--- dot_config/nvim/lua/common.lua
+++
@@ -1,8 +0,0 @@
-function nmap(mode, key, cmd, desc, buffer)
-    vim.keymap.set(mode, key, cmd, {
-        noremap = true,
-        silent = true,
-        desc = desc,
-        buffer = buffer,
-    })
-end
