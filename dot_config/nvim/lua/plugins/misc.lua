125e2d8cdf94fd876b408b57f8395158df39c69e
Richard Garai <garric@okki.hu>
2025-10-04 21:42:54 +0200

    add conform

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -4,3 +4,18 @@ later(function()
   add({ source = "NMAC427/guess-indent.nvim" })
   require("guess-indent").setup()
 end)
+
+later(function()
+  add( { source = "stevearc/conform.nvim" })
+  require("conform").setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+      rust = { "rustfmt" },
+      go = { "gofmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  })
+end)
