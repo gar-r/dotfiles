eb580f7ac81bcda327a51364975433e54a3a58f8
Richard Garai <garric@okki.hu>
2026-01-18 20:05:51 +0100

    niri systemd needs script

---
+++ dot_config/systemd/user/run_z_systemd_needs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+systemctl --user add-wants niri.service mako.service
+systemctl --user add-wants niri.service waybar.service
+systemctl --user add-wants niri.service swaybg.service
+systemctl --user add-wants niri.service swayidle.service
+systemctl --user daemon-reload
